7e63b96583f65382a5d43d8222f804f7f0c4dc43
apizzulli <anttt.pizzulli@gmail.com>
2021-10-28 22:09:33 -0400

    added login option in menu.

--- client/src/App.js
+++ client/src/App.js
@@ -30,6 +30,7 @@ const App = () => {
                     <Switch>
                         <Route path="/" exact component={HomeWrapper} />
                         <Route path="/register/" exact component={RegisterScreen} />
+                        
                         <Route path="/top5list/:id" exact component={WorkspaceScreen} />
                     </Switch>
                     <Statusbar />
